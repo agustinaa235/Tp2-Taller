7312ad5f2c7bf1a75b64c5cbef0871351b73f323
agustina <asegura@fi.uba.ar>
2020-11-03 13:44:15 -0300

    agrego pre y post condicenoes a la clase verificador

--- verificador.h
+++ verificador.h
@@ -4,10 +4,26 @@
 
 class Verificador{
       public:
+            /*
+              * inicializara al verificador dejandolo valido
+            */
             explicit Verificador(Grafo* grafo);
+            /*
+              * liberara al verificador junto a sus recursos
+            */
             ~Verificador();
+            /*
+              * devuelve true si pudo decetetar un ciclo sino false
+            */
             bool verificar_ciclo();
+            /*
+              * devuelve true si detecta instrucciones sin usar sino false
+            */
             bool verificar_instrucciones_sin_usar();
+            /*
+              * devuelve 1 si se detecto un ciclo, 2 si hay instrucciones
+              * sin usar o 3 si el programa esta bien
+            */
             int verificar_bpf();
       private:
             Grafo* grafo;
